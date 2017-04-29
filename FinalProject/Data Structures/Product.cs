d228a027765b338332c0e650d0857bdc5967db14
Edward Yurkovsky <eddiyur@gmail.com>
2017-04-29 14:45:44 +0300

    Init Tool Init Product

---
+++ FinalProject/Data Structures/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Data_Structures
+{
+    public class Product
+    {
+        
+        public string ProductID { get; set; }
+        public string PruductName { get; set; }
+        public double ProductCapacity { get; set; }
+        public Dictionary<Product,int> ProductTree { get; set; }
+
+        //public List<Process> ProductProcessList{get;set;}
+
+
+
+    }
+}
---
+++ FinalProject/Data Structures/Tool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Data_Structures
+{
+    public class Tool
+    {
+       public enum ToolStatuses
+        {
+            work,
+            notWork
+        };
+        public string ToolName { get; set; }
+        public ToolStatuses ToolStatus { get; set; }
+
+    }
+}
